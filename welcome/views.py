a2c669f12cf4c013cf18d6e3aee007ec3652d0c5
lior <liorc59@gmail.com>
2018-05-18 12:22:19 +0300

    ds

--- welcome/views.py
+++ welcome/views.py
@@ -13,14 +13,8 @@ def index(request):
     PageView.objects.create(hostname=hostname)
 
     return render(request, 'welcome/index.html', {
-        'hostname': hostname,
-        'database': database.info(),
-        'count': PageView.objects.count()
     })
 
 def health(request):
     return HttpResponse(PageView.objects.count())
-	
-@app.route('/sayhey')
-def display():
-    return "hello from python!"
+
